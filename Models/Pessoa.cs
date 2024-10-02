486f8dd7c5351e0000d3c9f406cc0e6158eaf32a
aliceclimas <alicecristinalima31@gmail.com>
2024-10-02 09:51:22 -0300

    Estrutura do Controller: Pessoa e Ficha

---
+++ Models/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace CasaRepousoWeb.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
