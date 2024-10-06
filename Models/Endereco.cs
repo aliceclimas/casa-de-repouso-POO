e6848a0deda0357c66515459101803240074bd1f
ani <129322594+anaalvesg@users.noreply.github.com>
2024-10-06 16:02:03 -0300

    criação model endereco

---
+++ Models/Endereco.cs
@@ -0,0 +1,9 @@
+namespace CasaRepousoWeb.Models
+{
+    public class Endereco
+    {
+        public int EnderecoId { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+    }
+}
