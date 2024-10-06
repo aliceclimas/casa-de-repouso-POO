2426ef6901e198b12d2f9fb076d53904a6456f22
ani <129322594+anaalvesg@users.noreply.github.com>
2024-10-06 16:43:08 -0300

    Update EnderecoController.cs

--- Controllers/EnderecoController.cs
+++ Controllers/EnderecoController.cs
@@ -30,7 +30,7 @@ public class EnderecoController : Controller
     public ActionResult Delete(int id) {
         var endereco = db.Enderecos.Single(e => e.EnderecoId == id); 
 
-        db.Endereco.Remove(endereco);
+        db.Enderecos.Remove(endereco);
         db.SaveChanges();
 
         return RedirectToAction("Read");
@@ -45,11 +45,11 @@ public class EnderecoController : Controller
     [HttpPost]
     public ActionResult Update(int id, Endereco model) {
         var endereco = db.Enderecos.Single(e => e.EnderecoId == id);
-        enderco.Rua = model.Rua;
-        enderco.Numero = model.Numero;
-        enderco.Bairro = model.Bairro;
-        enderco.CEP = model.CEP;
-        enderco.Complemento = model.Complemento;
+        endereco.Rua = model.Rua;
+        endereco.Numero = model.Numero;
+        endereco.Bairro = model.Bairro;
+        endereco.CEP = model.CEP;
+        endereco.Complemento = model.Complemento;
 
         db.SaveChanges();
 
