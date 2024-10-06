70c35e297c1eaeadbd93fa94c5c998b6a7a1890d
ani <129322594+anaalvesg@users.noreply.github.com>
2024-10-06 16:07:49 -0300

    update model endereco

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -5,5 +5,8 @@ namespace CasaRepousoWeb.Models
         public int EnderecoId { get; set; }
         public string Rua { get; set; }
         public string Bairro { get; set; }
+        public string Numero { get; set; }
+        public string CEP { get; set; }
+        public string Complemento { get; set; }
     }
 }
